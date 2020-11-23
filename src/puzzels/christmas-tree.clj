a332a34a43b3ca43dac9716864cdc4c2fab1f14d
Patrick van de Glind <patrick_de_glind@hotmail.com>
2020-11-23 20:53:46 +0100

    Christmas tree: add iterate and zipmap examples, working with collections

--- src/puzzels/christmas-tree.clj
+++ src/puzzels/christmas-tree.clj
@@ -49,9 +49,40 @@ tree-map-reduce
 (def tree-map-for (calculate-tree-for 6 80 14))
 tree-map-for
 
+; iterate, only generating the widths
+(defn calculate-tree-iterate
+  "returns a list with the width results"
+  [levels start-width decrease]
+  (take levels (iterate #(- % decrease) start-width)))
+
+(def tree-list-iterate (calculate-tree-iterate 6 80 14))
+tree-list-iterate
+
+(defn calculate-tree-iterate-pos
+  "while iterate returns a positive result, returns the width results"
+  [start-width decrease]
+  (take-while pos? (iterate #(- % decrease) start-width)))
+
+(def tree-list-iterate-pos (calculate-tree-iterate-pos 80 14))
+
+(count tree-list-iterate-pos) ; length
+
+; zipmap, generate 2 coll's from levels and iterate, then zipmap
+(defn calculate-tree-zipmap
+  [levels start-width decrease]
+  (let [lvl-coll (range levels)
+        calc-coll (iterate #(- % decrease) start-width)]
+    (zipmap lvl-coll calc-coll)))
+
+(def tree-map-zipmap (calculate-tree-zipmap 6 80 14))
+
 ; using reduce, destruct then sum
 (reduce (fn [s [_lvl length]] (+ s length)) 0 tree-map-for) ; reductions
 
 ; get the lengths (destruct) in coll and sum them up
 (apply + (map (fn [[_lvl length]] length) tree-map-for)) ; second value
 (apply + (map second tree-map-for)) ; same
+(apply + (map second tree-map-zipmap)) ; same
+
+(apply + tree-list-iterate)
+(apply + tree-list-iterate-pos)
