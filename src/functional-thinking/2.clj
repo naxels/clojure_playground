cd048eb98ae3afd2b71bac2cfa439b4393fabed4
Patrick van de Glind <patrick_de_glind@hotmail.com>
2021-02-27 12:20:47 +0100

    Functional Thinking ch2, 4

---
+++ src/functional-thinking/2.clj
@@ -0,0 +1,60 @@
+(ns functional-thinking.2)
+
+(defn classify [num]
+  (let [factors (->> (range 1 (inc num))
+                     (filter #(zero? (rem num %))))
+        sum (reduce + factors)
+        aliquot-sum (- sum num)]
+    (cond
+      (= aliquot-sum num) :perfect
+      (> aliquot-sum num) :abundant
+      (< aliquot-sum num) :deficient)))
+
+(map classify (range 1 100))
+
+(->> (range 1 10000)
+     (map classify)
+     (filter #(= :perfect %)))
+    ;;  count)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+
+(defn perfect-num-vec [nr]
+  [nr (classify nr)])
+
+(map #(perfect-num-vec %) (range 1 1000))
+
+;; https://en.wikipedia.org/wiki/Perfect_number#Odd_perfect_numbers
+
+(defn perfect-numbers
+  [until-nr]
+  (->> (range 1 until-nr)
+       (filter even?)
+       (map #(perfect-num-vec %))
+       (filter #(= :perfect (second %)))))
+
+(perfect-numbers 10000)
+; 10000
+;; [6 :perfect] [28 :perfect] [496 :perfect] [8128 :perfect])
+
+(def numbers (range 1 11))
+(filter #(zero? (rem % 3)) numbers)
+
+(def words ["the" "quick" "brown" "fox" "jumped" "over" "the" "lazy" "dog"])
+(filter #(= 3 (count %)) words)
+
+; get first result
+;; (some #(= 3 (count %)) words) ; returns true/false
+(some #(when (= 3 (count %)) %) words) ; returns value
+(first (filter #(= 3 (count %)) words)) ; returns value
+(take 1 (filter #(= 3 (count %)) words)) ; returns in col
+
+(reduce + (range 1 11))
+(apply + (range 1 11))
+
+; partials
+(def min-hundred (partial - 100))
+
+(def half (min-hundred 50))
+
+half
---
+++ src/functional-thinking/4.clj
@@ -0,0 +1,41 @@
+(ns functional-thinking.4
+  (:require [clojure.string :refer [join split]]))
+
+
+; rot13 with memoize
+(let [alpha (into #{} (concat (map char (range (int \a) (inc (int \z))))
+                              (map char (range (int \A) (inc (int \Z)))))) ; #{\A \a \B \b \C \c \D \d \E \e \F \f ...}
+      rot13-map (zipmap alpha ; zipmap outputs map with key value
+                        (take 52 ; take 52 from the point of dropping 26 items
+                              (drop 26 ; drop 26 from the list of infinite repetition
+                                    (cycle alpha))))] ; cycle just keeps repeating the coll infinitly
+;; (def a rot13-map)
+  (defn rot13
+    "Given an input string, produce the rot 13 version of the string. \"hello\" -> \"uryyb\""
+    [s]
+    (apply str (map #(get rot13-map % %) s))))
+
+(defn name-hash
+  [name]
+  (apply str (map #(rot13 %) (split name #"\d"))))
+
+(def name-hash-m (memoize name-hash)) ; memoizes the calculated values, so lookups are speed up
+
+(name-hash "hello")
+(name-hash-m "hello")
+(name-hash-m "heeeeeeelloooooooo")
+(name-hash-m "HEEEEEEELLOOOOOOOO")
+
+; palindrome
+(defn palindrome?
+  [s]
+  (let [sl (.toLowerCase s)]
+    (= sl (apply str (reverse sl)))))
+
+(defn find-palindromes
+  [s]
+  (filter palindrome? (split s #" ")))
+
+(find-palindromes "The quick brown fox jumped over anna the dog")
+(find-palindromes "Bob went to Harrah and gambled with Otto and Steve")
+(take 1 (find-palindromes "Bob went to Harrah with Otto and Steve"))
