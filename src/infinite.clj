9bd16a8bd15349da9911db0c912da98cadfd92d7
Patrick van de Glind <patrick_de_glind@hotmail.com>
2021-06-03 20:37:09 +0200

    Added more infinite examples/functions

--- src/infinite.clj
+++ src/infinite.clj
@@ -16,3 +16,47 @@
         "Emma" "mint"
         "Piet" "chocolate"}
        (give_candy data/candy data/kids)))
+
+;; repeat - lazy infinite (or n) sequence
+(take 5 (repeat "x"))
+; same as
+(repeat 5 "x")
+
+;; repeatedly - lazy infinitely (or n) call a function used for side effects like randoms
+(take 10 (repeatedly #(rand-int 11)))
+; same as
+(repeatedly 10 #(rand-int 100))
+
+;; iterate
+(take 5 (iterate inc 5))
+
+; generates a list of what happens when you get 5% interest on a principle of $1000
+(take 16 (iterate #(* % 1.05) 1000)) ; doubles in 15 years
+
+(nth (iterate #(* % 1.05) 1000) 15) ; just get the nth value
+
+;; range
+(take 10 (range))
+
+; this shows the lazyness of all the intermediate functions
+(->> (range) ; if you change it to a finite range (range 0 5) it will not crash, however return less than 10 results
+     (filter even?)
+     (map #(* 3 %))
+     (map str)
+     (take 10))
+
+; finite range
+(range -5 5)
+
+; with steps of 10
+(range -100 100 10)
+
+;; lazy-seq
+(defn positive-numbers
+  ([] (positive-numbers 1))
+  ([n]
+   (lazy-seq (cons n (positive-numbers (inc n))))))
+
+(take 50 (positive-numbers))
+
+(take 50 (positive-numbers 10))
