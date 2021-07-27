13f510fdd63c44176cd41cdef74eb3577964b593
Patrick van de Glind <patrick_de_glind@hotmail.com>
2021-07-27 22:23:33 +0200

    First try at converting multisubst to more Clojure-like

--- src/tracing.clj
+++ src/tracing.clj
@@ -5,6 +5,7 @@
 
 (trace/trace-ns 'tracing) ; trace entire namespace
 
+; port from Racket/Scheme
 ;; (trace/deftrace multisubst ; trace a specific function
 (defn multisubst
   [new old lat]
@@ -15,3 +16,32 @@
                 (multisubst new old (rest lat)))))
 
 (multisubst 'vegetables 'fruit '(the cats eat meat no fruit they do not like fruit apen))
+
+(defn multisubstclj
+  [new old lat]
+  (if (empty? lat)
+    '()
+    (if (= (first lat) old)
+      (cons new (multisubstclj new old (rest lat)))
+      (cons (first lat)
+            (multisubstclj new old (rest lat))))))
+
+(multisubstclj 'vegetables 'fruit '(the cats eat meat no fruit they do not like fruit apen))
+
+(defn multisubstclj-two
+  [new old lat]
+  (if (empty? lat)
+    '()
+    (let [new-or-first (if (= (first lat) old)
+                         new
+                         (first lat))] 
+         (cons new-or-first (multisubstclj-two new old (rest lat))))))
+
+(multisubstclj-two 'vegetables 'fruit '(the cats eat meat no fruit they do not like fruit apen))
+
+(defn length [collection]
+  (if (empty? collection)
+    0
+    (+ 1 (length (rest collection)))))
+
+(length [0 1 2 3])
