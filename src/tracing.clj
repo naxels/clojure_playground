36b57662c0d8e01b54d80981a0e541424187c9b0
Patrick van de Glind <patrick_de_glind@hotmail.com>
2021-07-29 17:44:22 +0200

    Added more ways to do multisubst
    and replace cons with conj, more clojure way

--- src/tracing.clj
+++ src/tracing.clj
@@ -38,9 +38,37 @@
     (let [f-lat (first lat)
           new-or-first (or (when (= f-lat old) new)
                            f-lat)]
-         (cons new-or-first (multisubstclj-two new old (rest lat))))))
+         (conj (multisubstclj-two new old (rest lat)) new-or-first))))
 
 (multisubstclj-two 'vegetables 'fruit '(the cats eat meat no fruit they do not like fruit apen))
+
+(defn multisubstclj-loop
+  [new old lat]
+  (loop [my-result '()
+         my-lat (reverse lat)] ; needed because we process by adding on the front
+    (if (empty? my-lat)
+      my-result
+      (recur (let [f-lat (first my-lat)
+                   new-or-first (or (when (= f-lat old) new)
+                                    f-lat)] (conj my-result new-or-first))
+             (rest my-lat)))))
+
+(multisubstclj-loop 'vegetables 'fruit '(the cats eat meat no fruit they do not like fruit apen))
+
+; using a map to replace a value with another
+(replace {'vegetables 'fruit} '(the cats eat meat no fruit they do not like fruit apen))
+
+; borrowing from the replace source code, doing a map makes a lot of sense since go over each value
+(map #(if-let [e (find {'vegetables 'fruit} %)] (val e) %) '(the cats eat meat no fruit they do not like fruit apen)) ; returns the val of map 'fruit
+
+; need to create a closure that returns the reduce function with old and new applied
+; tracing doesn't work on reduce, use reductions:
+(reductions (fn [so-far val]
+          (let [new-or-val (or (when (= val 'fruit) 'vegetables)
+                               val)]
+            (conj so-far new-or-val)))
+        '()
+        (reverse '(the cats eat meat no fruit they do not like fruit apen))) ; need to again use reverse
 
 (defn length [collection]
   (if (empty? collection)
