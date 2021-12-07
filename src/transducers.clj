4fa87eb236591b0df4567956df32738546498a91
Patrick van de Glind <patrick_de_glind@hotmail.com>
2021-12-07 21:24:40 +0100

    Transducers: made a start

---
+++ src/transducers.clj
@@ -0,0 +1,33 @@
+(ns transducers)
+
+; https://clojure.org/reference/transducers
+
+; my take: allows for reuse of composed function(s)
+; basically: define once, reuse everywhere
+; handy is: the composed function doesn't care about input or output
+; afterwards you can do a variety of things with the result of transduction
+
+; composed function:
+(def inc-odd
+  (comp
+   (map inc)
+   (filter odd?)))
+
+; transduce - will immediately (not lazily) reduce over coll
+(time (transduce inc-odd + (range 10000)))
+
+; equivalent to (but without composing):
+(time
+ (->> (range 10000)
+      (map inc)
+      (filter odd?)
+      (reduce +)))
+
+; with init val
+(transduce inc-odd + -250000 (range 1000))
+
+; using into
+(into [] inc-odd (range 10))
+
+; sequence - lazy transducing
+(sequence inc-odd (range 10))
