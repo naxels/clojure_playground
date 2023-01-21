b668b3454af822dac8d1e167422b34ad57969d67
Patrick van de Glind <patrick.glind@hitachivantara.com>
2023-01-21 23:44:52 +0100

    pivot ns: add note about Reshape from tablecloth lib

--- src/data_and_viz/pivot.clj
+++ src/data_and_viz/pivot.clj
@@ -17,3 +17,8 @@
 ; {:good {1 [1 2], 2 [5 6]}, 
 ;  :bad {1 [3 4], 2 [7 8]}}
 (pivot data/map-of-map-vec)
+
+; other interesting way is Reshape:
+; https://scicloj.github.io/tablecloth/index.html#Reshape
+;; pivot->longer
+;; pivot->wider
