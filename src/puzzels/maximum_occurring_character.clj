6c5fb2f976760c3505af30a44ea7cf970d7d4796
Patrick van de Glind <patrick.glind@hitachivantara.com>
2023-02-04 11:35:26 +0100

    puzzels - maximum-occurring-character add puzzle

--- src/puzzels/maximum_occurring_character.clj
+++ src/puzzels/maximum_occurring_character.clj
@@ -1,5 +1,24 @@
 (ns puzzels.maximum-occurring-character)
 
+;; HackerRank
+
+;; Given a string, return the character that appears the maximum number of times in the string. The string will contain only ASCII characters, from the ranges ('a'-'z','A'-'Z','0'-'9'), and case matters. If there is a tie in the maximum number of times a character appears in the string, return the character that appears first in the string.
+ 
+;; Example
+;; text = abbbaacc
+ 
+;; Both 'a' and 'b' occur 3 times in text.  Since 'a' occurs earlier, a is the answer.
+ 
+;; Function Description
+ 
+;; Complete the function maximumOccurringCharacter in the editor below.
+ 
+;; maximumOccurringCharacter has the following parameter:
+;;     string text:  the string to be operated upon
+ 
+;; Returns
+;;     char : The most occurring character that appears first in the string.
+
 ; Complete the 'maximumOccurringCharacter' function below.
 ;
 ; The function is expected to return a CHARACTER.
